c20afc91d8d4e27350b2fa48cfaa5816c00e2bf6
dansenjago <muhamadeldanesh@gmail.com>
2026-05-08 09:44:47 +0700

    Add files via upload

---
+++ member.js
@@ -0,0 +1,13 @@
+const members = [
+  {
+    name:"Expone",
+    role:"XII-1",
+    photo:"logokelas/1. expone.jpg",
+    photos:[
+      "photokelas/1. expone/MX3L1005.JPG",
+      "MX3L1008.JPG",
+      "MX3L1010.JPG"
+    ],
+    g:"g1"
+  }
+];
